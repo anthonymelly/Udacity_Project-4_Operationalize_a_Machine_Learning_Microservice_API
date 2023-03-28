2a50886e49731d13fa00be1571a6f0bc9853eabe
EC2 Default User <ec2-user@ip-172-31-70-61.ec2.internal>
2023-03-28 12:32:19 +0000

    modified project files

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=ramonarrellano/ramon1
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker --tag ramon1 $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
